1f628e8cfb1744690c3862bcfe5234ab75b7a206
Fardan11 <fardankabir11@gmail.com>
2024-01-08 21:29:10 +0600

    'Funtionality Added'

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.close-modal');
 
-const btnsOpenmoadal = document.querySelectorAll('.show-modal'); //If we dont use All it only selects the first element!!
-console.log(btnsOpenmoadal);
+const btnsOpenmodal = document.querySelectorAll('.show-modal'); //If we dont use All it only selects the first element!!
+console.log(btnsOpenmodal);
+const btnsClosemodal = document.querySelector('.close-modal'); //If we dont use All it only selects the first element!!
 
-for (let i = 0; i < btnsOpenmoadal.length; i++) {
-  btnsOpenmoadal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden'); // there is no dot here, fot is only for selector!!
-  });
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden'); // there is no dot here, fot is only for selector!!
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenmodal.length; i++) {
+  btnsOpenmodal[i].addEventListener('click', openModal);
 }
+btnsClosemodal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
